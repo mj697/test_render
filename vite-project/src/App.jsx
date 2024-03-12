3cc8511bf134b194e1f52a9f3cf95eae93b4caf8
Mohammad Janbazi <masterchef146@gmail.com>
2024-03-12 18:04:56 +0330

    lets see on render video part

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const myVideo = useRef();
+  const [stream, setStream] = useState();
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+      .then((currentStream) => {
+        setStream(currentStream);
+        if (myVideo.current) myVideo.current.srcObject = currentStream;
+        //if current is undefined then we cannot assign anything to lefthand side
+      });
+  }, [])
 
   return (
     <>
@@ -28,6 +39,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <video playsInline ref={myVideo} autoPlay />
+      </div>
     </>
   )
 }
